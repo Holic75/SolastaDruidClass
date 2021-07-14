e4c48380b5122e4083b3ed8251ea3d879091c6f9
DubhHerder <83278737+DubhHerder@users.noreply.github.com>
2021-07-14 23:05:07 +0100

    Update CircleOfShifters.cs

--- SolastaDruidClass/CircleOfShifters.cs
+++ SolastaDruidClass/CircleOfShifters.cs
@@ -6,19 +6,24 @@ using HarmonyLib;
 
 namespace SolastaDruidClass
 {
+
+Similar to symbiotic entity, 
+          Shepards spirit totem, 
+          circle of spirits, 
+       circle of the shifter
+
    Level 2
 3forms to choose from
 
 Best/boar defence
 Wolf/tiger attack
 Stag/etc support -> limited 10min battle aura
+Advantage on con savs
 
-Similar to symbiotic entity
-Shepards spirit totem
 Advantage on any interaction with beasts/limited form of animalfriendship,maybe 1 or 10 minutes?
 Level 6
 Extra attack in line with moon druid getting wildshpaes with multi attack
-
+Or add beast features like pack tactics, multi attack , spider climb , charge etc
 
 Level 10
 Two forms active at the same time
