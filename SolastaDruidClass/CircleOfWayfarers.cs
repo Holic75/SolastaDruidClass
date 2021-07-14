857ccfde2c5e865d8c5c19397aa94ed7ae8c49b1
DubhHerder <83278737+DubhHerder@users.noreply.github.com>
2021-07-14 22:04:56 +0100

    Update CircleOfWayfarers.cs

--- SolastaDruidClass/CircleOfWayfarers.cs
+++ SolastaDruidClass/CircleOfWayfarers.cs
@@ -7,8 +7,17 @@ using HarmonyLib;
 namespace SolastaDruidClass
 {
 
+Level 2 
+Wayfarer spirit companion - air based version of wildfire spirit
+Focusing on movement rather than fire damage
+Gust attack to shove enemies
+Give caster extra movement longer jump and advantage in restrained etc
+Movement based spell list
 
+Level 6 teleport allies (without fire damage so further distance to balance)
 
+Level 10 flight?
 
+Level 14 death ward to ends spirit?
 
 }
